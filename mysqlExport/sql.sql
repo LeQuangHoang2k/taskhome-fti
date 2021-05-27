66c08a7d453101ea0da7e163c2d9bbad98245836
your username <theibdn159555@gmail.com>
2021-05-27 09:11:31 +0700

    create mysqlExport

--- mysqlExport/sql.sql
+++ mysqlExport/sql.sql
@@ -37,3 +37,24 @@ create table Product(
 ALTER TABLE Cart ADD FOREIGN KEY (AccountId) REFERENCES Account(AccountId);
 ALTER TABLE OrderDetail ADD FOREIGN KEY (CartId) REFERENCES Cart(CartId);
 ALTER TABLE OrderDetail ADD FOREIGN KEY (ProductId) REFERENCES Product(ProductId);
+
+INSERT INTO Product (ProductType,ProductName,ProductImage,ProductPrice,ProductAmount) 
+	VALUES ('phone','Samsung Galaxy j2','samsung',3000000,50);
+INSERT INTO Product (ProductType,ProductName,ProductImage,ProductPrice,ProductAmount) 
+	VALUES ('phone','Iphone','iphone12',30000000,50);
+INSERT INTO Product (ProductType,ProductName,ProductImage,ProductPrice,ProductAmount) 
+	VALUES ('phone','redmi note 10','redmi10',10000000,50);
+    
+INSERT INTO Product (ProductType,ProductName,ProductImage,ProductPrice,ProductAmount) 
+	VALUES ('laptop','acer','acer',3000000,50);
+INSERT INTO Product (ProductType,ProductName,ProductImage,ProductPrice,ProductAmount) 
+	VALUES ('laptop','hp','hp',3000000,50);
+INSERT INTO Product (ProductType,ProductName,ProductImage,ProductPrice,ProductAmount) 
+	VALUES ('laptop','macbook','macbook',3000000,50);
+    
+INSERT INTO Product (ProductType,ProductName,ProductImage,ProductPrice,ProductAmount) 
+	VALUES ('screen','acer','acer',300000,50);
+INSERT INTO Product (ProductType,ProductName,ProductImage,ProductPrice,ProductAmount) 
+	VALUES ('screen','aorus','aorus',300000,50);
+INSERT INTO Product (ProductType,ProductName,ProductImage,ProductPrice,ProductAmount) 
+	VALUES ('screen','dell','dell',300000,50);
